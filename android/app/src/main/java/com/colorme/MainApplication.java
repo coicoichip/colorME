732f689cd029ded0bacc6d8d8ee4511bf335cb0b
coz <congvc198@gmail.com>
2018-05-05 10:59:51 +0700

    update

--- android/app/src/main/java/com/colorme/MainApplication.java
+++ android/app/src/main/java/com/colorme/MainApplication.java
@@ -3,8 +3,6 @@ package com.colorme;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
-import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +24,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
-            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new VectorIconsPackage()
       );
     }
