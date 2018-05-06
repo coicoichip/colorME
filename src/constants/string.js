3c5fc9830749fff89fa307c57161ba52d09be80f
Apple <apple@Apples-MacBook-Pro.local>
2018-05-06 14:17:56 +0700

    a

--- src/constants/string.js
+++ src/constants/string.js
@@ -1,7 +1,17 @@
 export const STRINGS = {
+    LOGO_COLOR: 'color',
+    LOGO_ME: 'ME',
     REGISTER: 'Register',
     NAME: 'Name',
     EMAIL: 'Email',
     USERNAME: 'Username',
-    PASSWORD: 'Password'
+    PASSWORD: 'Password',
+    HAVE_ERROR: 'Có lỗi xảy ra',
+    EMPTY_NAME: 'Bạn chưa nhập tên',
+    EMPTY_USERNAME: 'Bạn chưa nhập tên tài khoản',
+    EMPTY_PASSWORD: 'Bạn chưa nhập mật khẩu',
+    EMPTY_EMAIL: 'Bạn chưa nhập địa chỉ email',
+    WRONG_EMAIL: 'Địa chỉ email không hợp lệ',
+    REGISTER_ACCOUNT: 'Đăng ký tài khoản',
+    LOGIN: 'Đăng nhập'
 }
