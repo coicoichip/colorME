3e99e147f2c7abd14f476972d23f0f6f81e1d79c
Vuong Hai Thanh <coicoichip@gmail.com>
2018-05-15 15:47:19 +0700

    a

--- src/modules/schedule/scheduleApi.js
+++ src/modules/schedule/scheduleApi.js
@@ -2,8 +2,11 @@ import * as env from "../../constants/env";
 import { AsyncStorage } from "react-native";
 import axios from "axios";
 import { APIS } from "../../constants/env";
-export function getScheduleApi(token) {
-  let url = APIS.API_URL_UNMANAGE + "colorme.vn/api/v3/user/schedule?token=" + token;
-  //console.log(url);
+export async function getScheduleApi() {
+  let url = "";
+  await AsyncStorage.getItem("@UserToken").then(value => {
+    url =
+      APIS.API_URL_UNMANAGE + "colorme.vn/api/v3/user/schedule?token=" + value;
+  });
   return axios.get(url);
 }
